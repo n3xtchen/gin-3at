692fa25031dc0901c6d3a72fd4d012177647e71e
n3xtchen <echenwen@gmail.com>
2025-07-14 23:59:16 +0800

    增加用户登录接口-测试

--- internal/handler/handler_test.go
+++ internal/handler/handler_test.go
@@ -9,30 +9,6 @@ import (
 	"net/url"
 
 	"github.com/gin-gonic/gin"
-
-	e "github.com/n3xtchen/gin-3at/internal/domain/entity"
-)
-
-var (
-	orderMock = map[string]interface{}{
-		"amount": 100.0,
-		"address": map[string]interface{}{
-			"name":    "John Doe",
-			"phone":   "12345678901",
-			"address": "123 Main St",
-		},
-		"order_items": []map[string]interface{}{
-			{
-				"name":     "Item 1",
-				"price":    10.0,
-				"quantity": 2,
-			}, {
-				"name":     "Item 2",
-				"price":    20.0,
-				"quantity": 1,
-			},
-		},
-	}
 )
 
 func GetTestGinContext(w *httptest.ResponseRecorder) *gin.Context {
@@ -60,32 +36,4 @@ func MockJsonPost(c *gin.Context, content interface{}) {
 	// so you wrap it in a no-op closer
 	c.Request.Body = io.NopCloser(bytes.NewBuffer(jsonbytes))
 
-}
-
-type OrderServiceMock struct {
-}
-
-func (m *OrderServiceMock) CreateOrder(order *e.Order) error {
-	return nil // Default behavior if no mock function is set
-}
-
-func (m *OrderServiceMock) GetOrderDetail(orderID int) (*e.Order, error) {
-	order := &e.Order{
-		ID:     orderID,
-		Amount: 100.0,
-		Address: e.Address{
-			Name:    "John Doe",
-			Phone:   "12345678901",
-			Address: "123 Main St",
-		},
-		Items: []e.OrderItem{
-			{
-				ProductID: 1,
-				Quantity:  2,
-				Price:     10.0,
-			},
-		},
-	}
-
-	return order, nil
 }
--- internal/handler/order_test.go
+++ internal/handler/order_test.go
@@ -5,8 +5,59 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	e "github.com/n3xtchen/gin-3at/internal/domain/entity"
 	"github.com/n3xtchen/gin-3at/internal/dto"
 )
+
+var (
+	orderMock = map[string]interface{}{
+		"amount": 100.0,
+		"address": map[string]interface{}{
+			"name":    "John Doe",
+			"phone":   "12345678901",
+			"address": "123 Main St",
+		},
+		"order_items": []map[string]interface{}{
+			{
+				"name":     "Item 1",
+				"price":    10.0,
+				"quantity": 2,
+			}, {
+				"name":     "Item 2",
+				"price":    20.0,
+				"quantity": 1,
+			},
+		},
+	}
+)
+
+type OrderServiceMock struct {
+}
+
+func (m *OrderServiceMock) CreateOrder(order *e.Order) error {
+	return nil // Default behavior if no mock function is set
+}
+
+func (m *OrderServiceMock) GetOrderDetail(orderID int) (*e.Order, error) {
+	order := &e.Order{
+		ID:     orderID,
+		Amount: 100.0,
+		Address: e.Address{
+			Name:    "John Doe",
+			Phone:   "12345678901",
+			Address: "123 Main St",
+		},
+		Items: []e.OrderItem{
+			{
+				ProductID: 1,
+				Quantity:  2,
+				Price:     10.0,
+			},
+		},
+	}
+
+	return order, nil
+}
 
 func TestOrderHandler_Save(t *testing.T) {
 
